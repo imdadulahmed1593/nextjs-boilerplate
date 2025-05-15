3944c6abd6b3ba0ea7fdad99dd1927194dc1ae44
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-05-16 00:25:52 +0600

    feat: update package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div className="bg-black bg-home-img bg-cover bg-center">
+    <div className="bg-black bg-home-img bg-cover bg-center opacity-80">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
-        <h1>Client Facing Home Page</h1>
+        <h1 className="text-5xl text-white">Client Facing Home Page</h1>
+        <p className="mt-2 text-white">
+          For exploring the boilerplate, go to /home
+        </p>
       </main>
     </div>
   );
